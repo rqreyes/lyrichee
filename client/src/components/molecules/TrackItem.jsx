cf96d7828dc826cf85ff1f141c9be144d47363f7
Randy Reyes <rqreyes@gmail.com>
2020-06-27 16:23:03 -0700

    added album cover art

--- client/src/components/molecules/TrackItem.jsx
+++ client/src/components/molecules/TrackItem.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const TrackItem = ({ track }) => {
+  const albumDisplay = track.album
+    ? track.album.cover_art_url
+    : 'https://picsum.photos/200';
+
   return (
     <li>
-      <p>{track.titles.full}</p>
+      <img src={albumDisplay} alt='album art' width='200px' height='200px' />
+      <p>{track.artist.name}</p>
+      <p>{track.titles.featured}</p>
       <Link to={`/tracks/${track.id}`}>
         <button type='button'>View Lyrics</button>
       </Link>
