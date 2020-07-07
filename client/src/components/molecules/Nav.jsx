d4d35c79624a26c18db66d9acb75c8d313c8f6a4
Randy Reyes <rqreyes@gmail.com>
2020-07-07 13:15:31 -0700

    added styles to nav

--- client/src/components/molecules/Nav.jsx
+++ client/src/components/molecules/Nav.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import {
+  faArrowLeft,
+  faStar,
+  faSignInAlt,
+} from '@fortawesome/free-solid-svg-icons';
 
 const Nav = () => {
   return (
     <nav>
-      <FontAwesomeIcon icon={faBars} />
+      <div className='button-group'>
+        <button type='button'>
+          <FontAwesomeIcon icon={faArrowLeft} />
+        </button>
+        <button type='button'>
+          <FontAwesomeIcon icon={faStar} />
+        </button>
+        <button type='button'>
+          <FontAwesomeIcon icon={faSignInAlt} />
+        </button>
+      </div>
     </nav>
   );
 };
