53920518b2a32cd3d42941d1702b44103d0e6c6c
Randy Reyes <rqreyes@gmail.com>
2020-07-10 11:14:53 -0700

    fixed landing page styles

--- client/src/components/organisms/Header.jsx
+++ client/src/components/organisms/Header.jsx
@@ -16,9 +16,7 @@ const Header = ({ landing }) => {
 
   const menuDisplay = toggleOpen ? <Nav toggleOpen={toggleOpen} /> : <Menu />;
   const headerDisplay = landing ? (
-    <header className='landing-header'>
-      <Nav />
-    </header>
+    <header className='landing-header'>{menuDisplay}</header>
   ) : (
     <Headroom>
       <header>
